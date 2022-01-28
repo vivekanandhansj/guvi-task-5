465111457b3da095b43db3570de496687aabaf3d
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-01-28 11:09:53 +0000

    Delete script.js

--- annonymous function/script.js
+++
@@ -1,5 +0,0 @@
-let a = function(str) { 
-   str = str.split(" ").map(([firstChar, ...rest]) => firstChar.toUpperCase() + rest.join("").toLowerCase()).join(" ");
-    console.log(str);
-  }
-  a("john smith");
