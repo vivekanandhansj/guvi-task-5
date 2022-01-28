3a1c672a66d16531b6df80bdc05b820b7af9f528
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-01-28 11:08:23 +0000

    Convert all the strings to title caps in a string array
     anonymous function

--- annonymous function/script.js
+++ annonymous function/script.js
@@ -1,8 +1,5 @@
-let b = function(a)  {
-   for (let i = 0; i<a.length; i++){
-       if(a[i] % 2 != 0 ){
-       console.log(a[i])
-   }
- }
-}
-b([1,2,3,4,5,6])
+let a = function(str) { 
+   str = str.split(" ").map(([firstChar, ...rest]) => firstChar.toUpperCase() + rest.join("").toLowerCase()).join(" ");
+    console.log(str);
+  }
+  a("john smith");
